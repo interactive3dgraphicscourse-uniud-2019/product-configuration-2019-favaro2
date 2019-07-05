e677b09b923abc6850833c1b3ce0285e20477de8
mauriziofavaro <mauriziofavaro1994@libero.it>
2019-07-05 16:10:39 +0200

    sistemazione codice calcolatore prezzo

--- scripts/priceCalculator.js
+++ scripts/priceCalculator.js
@@ -2,7 +2,7 @@ var doorPrice = 200;
 var handlePrice = 230;
 
 function calcPrice(){
-  var price = doorPrice + handlePrice
+  var price = doorPrice + handlePrice;
   document.getElementById('price').innerHTML = "";
   document.getElementById('price').innerHTML = "Prezzo: " + price + " €";
 }
